bfa252826e051ffa7355a8b22c13f7c0ce8c5811
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-04 17:00:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./styles.css";
 import Header from './components/Header/Header';
+import Summary from './components/Summary/Summary';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Summary />
     </div>
   );
 }
---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Summary.css';
+
+const Summary = () => {
+  return(
+    <div className='summary'>
+      <h1 className='heading'>
+        Explore Foods from<br />
+        around the Globe 
+      </h1>
+      <p className='summary-para'>
+      Whether you're looking for healthy recipes, or ideas on
+      how to use leftovers from your fridge, we've numerous
+      recipes to choose from, so you'll be able to find the
+      perfect dish.
+      </p>
+    </div>
+  )
+}
+
+export default Summary;
