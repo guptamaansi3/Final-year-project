bf1a8b763e04be94887c02a9be14156688d76e80
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-05 16:25:59 +0530

    initial

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import './Summary.css';
+import React from "react";
+import "./Summary.css";
 
 const Summary = () => {
-  return(
-    <div className='summary'>
-      <div className='heading-summary'>
-      <h1 className='heading'>
-        Explore Foods from<br />
-        around the Globe 
-      </h1>
+  return (
+    <div className="summary">
+      <div className="heading-summary">
+        <h1 className="heading">
+          Explore Foods from
+          <br />
+          around the Globe
+        </h1>
       </div>
-      <div className='paragraph'>
-      <p className='summary-para'>
-      Whether you're looking for healthy recipes, or ideas on<br />
-      how to use leftovers from your fridge, we've numerous<br />
-      recipes to choose from, so you'll be able to find the<br />
-      perfect dish.
-      </p>
+      <div className="paragraph">
+        <p className="summary-para">
+          Whether you're looking for healthy recipes, or ideas on
+          <br />
+          how to use leftovers from your fridge, we've numerous
+          <br />
+          recipes to choose from, so you'll be able to find the
+          <br />
+          perfect dish.
+        </p>
       </div>
-      <img src='assets/food.svg' alt='random' />
+      <img className="food-png" src="assets/food.svg" alt="random" />
     </div>
-  )
-}
+  );
+};
 
 export default Summary;
