7a9d3e2d40cfa9fb7e811e46339d91f879781a52
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-05 11:48:24 +0530

    initial commit

---
+++ src/components/Search-button/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Search.css';
+
+const Search = () => {
+  return(
+    <div className='search-button'>
+      <button className='button'>Search Recipes</button>
+    </div>
+  )
+}
+
+export default Search;
