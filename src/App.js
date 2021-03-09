7b15fd1f0297874929f2df762e544f5b2f31b184
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-09 14:32:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import HomePage from './components/HomePage/HomePage';
 import {Route, Switch} from 'react-router-dom';
-import Recipes from './components/RecipesPage/Recipes';
 import SavedRecipes from './components/Saved-Recipes/SavedRecipes';
 import "./styles.css";
+import Categories from "./components/RecipesPage/Categories";
 
 export default function App() {
   return (
     <Switch>
     <div className="App">
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/recipes' component={Recipes} />
+      <Route exact path='/recipes' component={Categories} />
       <Route exact path='/saved-recipes' component={SavedRecipes} />
     </div>
     </Switch>
