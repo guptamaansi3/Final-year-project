1b9ebde8f8eae2816814a7250a478afecbebb205
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-09 17:15:38 +0530

    initial commit

--- src/components/RecipesPage/Categories.js
+++ src/components/RecipesPage/Categories.js
@@ -3,12 +3,15 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './Categories.css';
 
+
 const Categories = () => {
   return(
-    <div className='categories-page'>
+    <div className='categories-wrapper'>
       <Header />
-      <div className='categories'>
-      <h1>Categories</h1>
+      <div className='hero-banner'>
+        <h1 className='hero-heading'>Search through 1.7+ million recipes</h1>
+        <input className='input-search' type='search' placeholder='    Recipe  Search' value='' />
+        <img className='search-svg' src='assets/search.svg' alt='random' />
       </div>
       <Footer />
     </div>
