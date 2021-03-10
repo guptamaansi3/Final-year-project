93ab0afd454bb338d708ebfdcb92c446671f8df4
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-10 11:12:05 +0530

    initial commit

--- src/components/RecipesPage/Categories.js
+++ src/components/RecipesPage/Categories.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import './Categories.css';
-
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import "./Categories.css";
 
 const Categories = () => {
-  return(
-    <div className='categories-wrapper'>
+  return (
+    <div className="categories-wrapper">
       <Header />
-      <div className='hero-banner'>
-        <h1 className='hero-heading'>Search through 1.7+ million recipes</h1>
-        <input className='input-search' type='search' placeholder='    Recipe  Search' value='' />
-        <img className='search-svg' src='assets/search.svg' alt='random' />
+      <div className="hero-banner">
+        <h1 className="hero-heading">Search through 1.7+ million recipes</h1>
+        <input
+          id="search-input"
+          className="input-search"
+          type="search"
+          placeholder="Recipe Search"
+        />
+        <img
+          id="search-button"
+          onclick="javascript.alert(input)"
+          className="search-svg"
+          src="assets/search.svg"
+          alt="random"
+        />
       </div>
-      <h1 className='meal-heading'>Meals of the day</h1>
+      <h1 className="meal-heading">Meals of the day</h1>
       <Footer />
     </div>
-  )
-}
+  );
+};
+
+var input = document.getElementById("search-input");
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("search-button").click();
+  }
+});
 
 export default Categories;
