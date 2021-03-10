f8ab086091349e2f9d3ae95835acaa017572c5ed
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-10 16:55:19 +0530

    initial commit

--- src/components/RecipesPage/CategoryCard.js
+++ src/components/RecipesPage/CategoryCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const CategoryCard = () => {
+const CategoryCard = (props) => {
   return(
       <button className='category-card'>
-        <div className='category-content'><
+        <div className='category-content'>
+          <img className='category-img' src={props.image} alt='food-category'>
+          <h3 className='category-title'>{props.title}</h3>
+        </div>
       </button>
-    
-  )
+     )
 }
 
-export default CategoryCard
+export default CategoryCard;
