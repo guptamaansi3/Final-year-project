db177bb13a6535f475cd430c6db1921f07b3b8a2
guptamaansi3 <79892174+guptamaansi3@users.noreply.github.com>
2021-03-11 10:56:25 +0530

    initial commit

---
+++ src/components/RecipesPage/CategoryData/HealthyMeals.jsx
@@ -0,0 +1,20 @@
+const healthy_meals_Data = [
+  {
+    imgsrc : 'assets/vegan.jpg',
+    title: 'Vegan'
+  },
+  {
+    imgsrc : 'assets/vegatarian.jpg',
+    title: 'Vegetarian'
+  },
+  {
+    imgsrc : 'assets/low-sugar.jpg',
+    title: 'Low Sugar'
+  },
+  {
+    imgsrc : 'assets/nut-free.jpg',
+    title: 'Nut Free'
+  }
+]
+
+export deafult healthy_meals_Data;
